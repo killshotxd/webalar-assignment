544eccc2c2e0c44066285d676b65bd25a91d9a58
killshotxd <hassanansari211@gmail.com>
2022-09-09 10:14:56 +0530

    section 1

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,34 @@
 import React from "react";
 import Header from "../Header/Header";
 import styles from "./Home.module.css";
+import unsplash from "../../assets/unsplash1.svg";
 const Home = () => {
   return (
     <>
       <Header />
+
+      <div className={styles.container}>
+        <div className={styles.left}>
+          <div className={styles.heading}>
+            {" "}
+            <h1>Let's create something great together</h1>
+          </div>
+
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero nisi
+            ipsam excepturi autem, veniam temporibus dolor eum omnis numquam
+            est, officiis vitae minima eveniet fuga nesciunt deleniti qui porro
+            fugiat. Nulla distinctio neque reiciendis!
+          </p>
+
+          <a className={styles.btn} href="#">
+            Lets Talk
+          </a>
+        </div>
+        <div className={styles.right}>
+          <img src={unsplash} alt="" />
+        </div>
+      </div>
     </>
   );
 };
