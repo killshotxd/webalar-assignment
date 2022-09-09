6cd11392cd74bf74179edda267d0e408a6f49795
killshotxd <hassanansari211@gmail.com>
2022-09-09 17:19:09 +0530

    sectio

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import Header from "../Header/Header";
 import styles from "./Home.module.css";
 import unsplash from "../../assets/unsplash1.svg";
+
+import { Navigation, Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import vector1 from "../../assets/vector1.svg";
 const Home = () => {
   return (
     <>
@@ -27,6 +33,32 @@ const Home = () => {
         </div>
         <div className={styles.right}>
           <img src={unsplash} alt="" />
+        </div>
+      </div>
+
+      <div className={styles.container2}>
+        <div className={styles.rectangle}>
+          <div className={styles.top}>
+            <div className={styles.line}></div>
+            <div className={styles.heading2}>Trusted Companies</div>
+            <div className={styles.line}></div>
+          </div>
+
+          <div className={styles.slider}>
+            <Swiper
+              modules={[Navigation]}
+              spaceBetween={50}
+              slidesPerView={3}
+              navigation
+              onSlideChange={() => console.log("slide change")}
+              onSwiper={(swiper) => console.log(swiper)}
+            >
+              <SwiperSlide>Slide 1</SwiperSlide>
+              <SwiperSlide>Slide 2</SwiperSlide>
+              <SwiperSlide>Slide 3</SwiperSlide>
+              <SwiperSlide>Slide 4</SwiperSlide>
+            </Swiper>
+          </div>
         </div>
       </div>
     </>
